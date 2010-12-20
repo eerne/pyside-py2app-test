125827b4025fbb1adc61fe6070fc1616fd66622d
Enrique Erne <enrique.erne@mild.ch>
2010-12-20 16:25:20 +0100

    inital commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	app = ['test.py'],
+	options = {
+		'py2app': {
+			'argv_emulation': False,
+			"""
+			'plist': {
+				'LSPrefersPPC': False,
+			},
+			"""
+			'includes': ['PySide.QtCore', 'PySide.QtGui', 'PySide.QtWebKit', 'PySide.QtNetwork'],
+		}
+	},
+	setup_requires = ['py2app'],
+)
